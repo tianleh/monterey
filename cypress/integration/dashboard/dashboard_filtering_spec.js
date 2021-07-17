e006c671a6651c95698b38c00cd930b147911888
Aviv Benchorin <benchori@amazon.com>
2021-07-17 10:32:56 -0700

    Add more tests to dashboard_filtering

--- cypress/integration/dashboard/dashboard_filtering_spec.js
+++ cypress/integration/dashboard/dashboard_filtering_spec.js
@@ -105,7 +105,7 @@ describe('dashboard filtering', () => {
 
     it('goal and guages are filtered', () => {
       // Goal label should be 0, gauge label should be 0%
-      cy.get('svg > g > g > text.chart-label').each(($el, index, $list) => {
+      cy.get('svg > g > g > text.chart-label').should('be.length', 2).each(($el, index, $list) => {
         cy.get($el).contains(/^0%?$/)
       })
     })
@@ -123,19 +123,25 @@ describe('dashboard filtering', () => {
     })
 
     it('tag cloud values are filtered', () => {
-      // cy.get('svg > g > g.series').should('not.exist')
+      cy.get('[data-test-subj="tagCloudVisualization"]').each(($el, index, $list) => {
+        cy.get($el).find('svg > g > text').should('not.exist')
+      })
     })
 
     it('tsvb metric is filtered', () => {
-      // cy.get('svg > g > g.series').should('not.exist')
+      cy.get('[data-test-subj="tsvbMetricValue"]').each(($el, index, $list) => {
+        cy.get($el).contains(/^0 custom template$/)
+      })
     })
 
     it('tsvb top n is filtered', () => {
-      // cy.get('svg > g > g.series').should('not.exist')
+      cy.get('[data-test-subj="tsvbTopNValue"]').should('be.length', 2).each(($el, index, $list) => {
+        cy.get($el).contains(/^0$/)
+      })
     })
 
     it('saved search is filtered', () => {
-      // cy.get('svg > g > g.series').should('not.exist')
+      cy.get('[data-test-subj="docTableExpandToggleColumn"]').should('not.exist')
     })
 
     // TODO: Uncomment once https://github.com/elastic/kibana/issues/22561 is fixed
@@ -144,24 +150,157 @@ describe('dashboard filtering', () => {
     // })
 
     it('vega is filtered', () => {
-      // cy.get('svg > g > g.series').should('not.exist')
+      cy.get('.vgaVis__view text').each(($el, index, $list) => {
+        cy.get($el).should('not.contain', /^5,000$/)
+      })
     })
   })
 
   describe('using a pinned filter that excludes all data', () => {
     before(() => {
+      cy.get('[data-test-subj="filter filter-enabled filter-key-bytes filter-value-12,345,678 filter-unpinned "]').click()
+      cy.get('[data-test-subj="pinFilter"]').click()
+    })
 
+    after(() => {
+      cy.get('[data-test-subj="filter filter-enabled filter-key-bytes filter-value-12,345,678 filter-pinned "]').click()
+      cy.get('[data-test-subj="pinFilter"]').click()
+    })
+
+    it('filters on pie charts', () => {
+      // Check that none of the pie charts are occupied with data (show "No results found")
+      cy.get('svg > g > g.arcs > path.slice').should('not.exist')
+    })
+
+    it('area, bar and heatmap charts filtered', () => {
+      // Check that none of the charts are filled with data
+      cy.get('svg > g > g.series').should('not.exist')
+    })
+
+    it('data tables are filtered', () => {
+      // Check that none of the data tables are filled with data
+      cy.get('[data-test-subj="paginated-table-body"] [data-cell-content]').should('not.exist')
+    })
+
+    it('goal and guages are filtered', () => {
+      // Goal label should be 0, gauge label should be 0%
+      cy.get('svg > g > g > text.chart-label').should('be.length', 2).each(($el, index, $list) => {
+        cy.get($el).contains(/^0%?$/)
+      })
+    })
+
+    it('metric value shows no data', () => {
+      // The metrics should show '-'
+      cy.get('.mtrVis__value').each(($el, index, $list) => {
+        cy.get($el).contains(/^ - $/)
+      })
+    })
+
+    it('tag cloud values are filtered', () => {
+      cy.get('[data-test-subj="tagCloudVisualization"]').each(($el, index, $list) => {
+        cy.get($el).find('svg > g > text').should('not.exist')
+      })
+    })
+
+    it('tsvb metric is filtered', () => {
+      cy.get('[data-test-subj="tsvbMetricValue"]').each(($el, index, $list) => {
+        cy.get($el).contains(/^0 custom template$/)
+      })
+    })
+
+    it('tsvb top n is filtered', () => {
+      cy.get('[data-test-subj="tsvbTopNValue"]').should('be.length', 2).each(($el, index, $list) => {
+        cy.get($el).contains(/^0$/)
+      })
+    })
+
+    it('saved search is filtered', () => {
+      cy.get('[data-test-subj="docTableExpandToggleColumn"]').should('not.exist')
+    })
+
+    // TODO: Uncomment once https://github.com/elastic/kibana/issues/22561 is fixed
+    // it('timeline is filtered', async () => {
+    //   await dashboardExpect.timelineLegendCount(0)
+    // })
+
+    it('vega is filtered', () => {
+      cy.get('.vgaVis__view text').each(($el, index, $list) => {
+        cy.get($el).should('not.contain', /^5,000$/)
+      })
     })
   })
 
   describe('disabling a filter unfilters the data on', () => {
     before(() => {
-
+      cy.get('[data-test-subj="filter filter-enabled filter-key-bytes filter-value-12,345,678 filter-unpinned "]').click()
+      cy.get('[data-test-subj="deleteFilter"]').click()
+      cy.get('[data-test-subj="filter filter-enabled filter-key-bytes filter-value-12,345,678 filter-unpinned "]').should('not.exist')
     })
-    // cy.get('[data-test-subj="filter filter-enabled filter-key-bytes filter-value-12,345,678 filter-unpinned "]').click()
-    // cy.get('[data-test-subj="deleteFilter"]').click()
-    // cy.get('[data-test-subj="filter filter-enabled filter-key-bytes filter-value-12,345,678 filter-unpinned "]').should('not.exist')
-    // cy.get('svg > g > g.series').should('exist')
+
+    it('pie charts', () => {
+      // Check that there are 5 slice in the  pie charts
+      cy.get('svg > g > g.arcs > path.slice').should('be.length', 5)
+    })
+
+    it('area, bar and heatmap charts', () => {
+      // Check that there are 3 charts
+      cy.get('svg > g > g.series').should('be.length', 3)
+    })
+
+    it('data tables', () => {
+      // Check that there are 10 table rows
+      cy.get('[data-test-subj="paginated-table-body"] [data-cell-content]').should('be.length', 10)
+    })
+
+    it('goal and guages', () => {
+      // Goal label should be 7,544, gauge label should be 39.958%%
+      cy.get('svg > g > g > text.chart-label').should('be.length', 2).each(($el, index, $list) => {
+        // to do
+        // cy.get($el).contains(/^0%?$/)
+      })
+    })
+
+    it('metric value', () => {
+      // The metrics should show '101'
+      cy.get('.mtrVis__value').each(($el, index, $list) => {
+        cy.get($el).contains(/^101$/)
+      })
+    })
+
+    it('tag cloud', () => {
+      cy.get('[data-test-subj="tagCloudVisualization"]').each(($el, index, $list) => {
+        // cy.get($el).find('svg > g > text').contains(['9,972', '4,886', '1,944', '9,025'])
+      })
+    })
+
+    it('tsvb metric', () => {
+      cy.get('[data-test-subj="tsvbMetricValue"]').each(($el, index, $list) => {
+        cy.get($el).contains(/^50,465 custom template$/)
+      })
+    })
+
+    it('tsvb top n', () => {
+      cy.get('[data-test-subj="tsvbTopNValue"]').should('be.length', 2).each(($el, index, $list) => {
+        // cy.get($el).contains(['6,308.125', '6,308.125'])
+      })
+    })
+    it('tsvb markdown', async () => {
+      // dashboardExpect.tsvbMarkdownWithValuesExists(['7,209.286']);
+    })
+    it('saved search is filtered', () => {
+      cy.get('[data-test-subj="docTableExpandToggleColumn"]').should('be.length', 1)
+    })
+
+    // TODO: Uncomment once https://github.com/elastic/kibana/issues/22561 is fixed
+    // it('timeline is filtered', async () => {
+    //   await dashboardExpect.timelineLegendCount(0)
+    // })
+
+    it('vega is filtered', () => {
+      cy.get('.vgaVis__view text').each(($el, index, $list) => {
+        // scy.get($el).contains(/^5,000$/)
+      })
+    })
   })
 
   describe('nested filtering', () => {
